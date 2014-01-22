02054e6c6165cf5b32f14b8ec006eed6f1bbfd5f
jwang92 <j.wang-16@student.tudelft.nl>
2014-01-22 13:10:43 +0100

    no message

--- server.php
+++ server.php
@@ -3,13 +3,30 @@ include_once("db.php");
 
 if (isset($_REQUEST['mode']) && $_REQUEST['mode']=='getArtikel'){
 	$art = $_REQUEST['artikel'];
-	//Request all the information from an article
 	
+	//Request all the information from an article
+	$artikel = mysql_query("SELECT * FROM artikel WHERE art = '".$art."'");
+	$afd = mysql_query("SELECT afd FROM verkart WHERE art = '".$art."'");
+	$result = mysql_fetch_row($artikel);
+	
+	foreach ($result as $value)
+		echo $value.';';
+	
+	while ($row = mysql_fetch_row($afd))
+		echo $row[0].';';
 	
 }
 
 else if (isset($_REQUEST['mode']) && $_REQUEST['mode']=='getKlant'){
+	$kl = $_REQUEST['klant'];
+	
 	//Request all the information from an customer
+	$klant = mysql_query("SELECT * FROM klant WHERE klant = '".$kl."'");
+	$result = mysql_fetch_row($klant);
+	
+	foreach ($result as $value)
+		echo $value.';';
+	
 }
 
 
